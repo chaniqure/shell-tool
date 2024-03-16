1b7b60ca65a447a20adfffc0e42e0fe864b82673
cc <1255861908@qq.com>
2024-03-17 00:22:29 +0800

    优化脚本

--- main.sh
+++ main.sh
@@ -20,20 +20,20 @@ while :; do echo
         esac
 done
 while :; do echo
-        echo '请选择操作:'
-        echo -e "\t1. 系统依赖环境处理"
-        echo -e "\t2. 帮助"
-        read -p "请输入操作编号：" option
-        case "$option" in
-            1)
-                clear
-                bash -c "$(curl -sLk $prefix/system_init.sh)"
-                ;;
-            2)
-                clear
-                bash -c "$(curl -sLk $prefix/help.sh)"
-                ;;
-            *)
-                echo -e " \033[1m \033[31m 输入参数错误，请重试 \033[0m"
-            esac
-    done
+    echo '请选择操作:'
+    echo -e "\t1. 系统依赖环境处理"
+    echo -e "\t2. 帮助"
+    read -p "请输入操作编号：" option
+    case "$option" in
+        1)
+            clear
+            bash -c "$(curl -sLk $prefix/system_init.sh)"
+            ;;
+        2)
+            clear
+            bash -c "$(curl -sLk $prefix/help.sh)"
+            ;;
+        *)
+            echo -e " \033[1m \033[31m 输入参数错误，请重试 \033[0m"
+        esac
+done
