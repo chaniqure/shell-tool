cd1e225123f03709e647b0ca7d2274fc6a652db6
cc <1255861908@qq.com>
2024-03-17 01:12:44 +0800

    优化脚本

--- main.sh
+++ main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# 国内加速脚本地址
+innerPrefix="https://fastraw.ixnic.net/chaniqure/shell-tool/main"
+# 国外GitHub官方脚本地址
+outerPrefix="https://raw.githubusercontent.com/chaniqure/shell-tool/main"
+
+# 执行脚本地址变量
 prefix=""
 while :; do echo
     echo '请选择安装环境:'
@@ -8,11 +14,11 @@ while :; do echo
     read -p "请输入操作编号：" env
     case "$env" in
         1)
-            prefix="https://fastraw.ixnic.net/chaniqure/shell-tool/main"
+            prefix=$innerPrefix
             break
             ;;
         2)
-            prefix="https://raw.githubusercontent.com/chaniqure/shell-tool/main"
+            prefix=$outerPrefix
             break
             ;;
         *)
@@ -27,12 +33,12 @@ while :; do echo
     case "$option" in
         1)
             clear
-            # echo -e " \033[1m \033[32m 执行地址为：$prefix/system_init.sh \033[0m"
+            echo -e " \033[1m \033[32m 执行地址为：$prefix/system_init.sh \033[0m"
             bash -c "$(curl -sLk $prefix/system_init.sh)"
             ;;
         2)
             clear
-            # echo -e " \033[1m \033[32m 执行地址为：$prefix/help.sh \033[0m"
+            echo -e " \033[1m \033[32m 执行地址为：$prefix/help.sh \033[0m"
             bash -c "$(curl -sLk $prefix/help.sh)"
             ;;
         *)
