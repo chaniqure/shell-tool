e3c1caa7618a0e9eb61f2ecb4cdfa90a46956c84
cc <1255861908@qq.com>
2024-03-16 23:59:38 +0800

    优化命令行提示

--- main.sh
+++ main.sh
@@ -7,17 +7,17 @@ function echoError(){
 
 while :; do echo
     echo '请选择操作:'
-    echo "\t1. 系统依赖环境处理"
-    echo "\t2. 帮助"
+    echo "  1. 系统依赖环境处理"
+    echo "  2. 帮助"
     read -p "请输入操作编号：" option
     case "$option" in
         1)  
             clear
-            bash -c "$(curl -sLk https://fastly.jsdelivr.net/gh/chaniqure/shell-tool@main/system_init.sh)"
+            bash -c "$(curl -sLk https://raw.githubusercontent.com/chaniqure/shell-tool/main/system_init.sh)"
             ;;
         2)
             clear
-            bash -c "$(curl -sLk https://fastly.jsdelivr.net/gh/chaniqure/shell-tool@main/help.sh)"
+            bash -c "$(curl -sLk https://raw.githubusercontent.com/chaniqure/shell-tool/main/help.sh)"
             ;;
         *)
             echoError '输入参数错误，请重试'
