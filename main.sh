1226e0ea586a6d177ef0c6413a6bb29ac911c24d
cc <1255861908@qq.com>
2024-03-19 10:22:46 +0800

    添加注释

--- main.sh
+++ main.sh
@@ -46,11 +46,11 @@ function main() {
                 ;;
             2)
                 clear
-                bash -c "$(curl -sLk $prefix/generate_env.sh)" $prefix
+                bash -c "$(curl -sLk $prefix/generate_env.sh)" _ $prefix
                 ;;
             3)
                 clear
-                bash -c "$(curl -sLk $prefix/generate_service.sh)" $prefix
+                bash -c "$(curl -sLk $prefix/generate_service.sh)" _ $prefix
                 ;;
             4)
                 clear
